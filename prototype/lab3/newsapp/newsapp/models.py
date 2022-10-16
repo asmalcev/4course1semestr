4a4b549f27d3d0f8d20398c292f35e45b3b9ce17
qzerrty <qzerrty@protonmail.com>
2022-10-16 23:49:16 +0300

    propotype:lab3: +authors

--- prototype/lab3/newsapp/newsapp/models.py
+++ prototype/lab3/newsapp/newsapp/models.py
@@ -1,6 +1,9 @@
+from email.policy import default
+from pyexpat import model
 from django.db import models
 from datetime import datetime
 from django.contrib import admin
+from django.contrib.auth.models import User
 
 
 
@@ -8,6 +11,7 @@ class News(models.Model):
 	publication_date = models.DateTimeField(default=datetime.now, blank=True)
 	title = models.CharField(max_length=128)
 	content = models.TextField(max_length=1024)
+	author = models.ForeignKey(User, on_delete=models.SET_NULL, blank=True, null=True)
 
 	def __str__(self):
 		return '{} {}'.format(self.publication_date.date(), self.title)
